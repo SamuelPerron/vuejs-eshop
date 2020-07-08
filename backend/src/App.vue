c2595f651eba03f77888310f0f4b89cb84c95af4
Samuel Perron <samuelperron@hotmail.fr>
2020-07-08 17:45:22 -0400

    Admin interface (base)

---
+++ backend/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="app">
+        <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+        <router-view/>
+    </div>
+</template>
+
+<script>
+import './assets/css/main.scss';
+
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+Vue.use(VueAxios, axios)
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
+export default {
+    name: 'App',
+    created() {
+        AOS.init();
+    }
+}
+</script>
